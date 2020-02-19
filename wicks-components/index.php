137a005a223bfe42ea8b596f2d151afef05fa81e
wstewiii <50386657+wstewiii@users.noreply.github.com>
2020-02-19 18:54:45 -0500

    Added home page and session vars

---
+++ wicks-components/index.php
@@ -0,0 +1,114 @@
+<?php 
+
+//Start session and check logon status
+session_start();
+if (isset($_SESSION['loggedon'])) {
+	$loggedon = $_SESSION['loggedon'];
+}
+else {
+	$loggedon = FALSE;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Title goes here">
+    <meta name="keywords"
+        content="Keywords go here">
+    <meta name="description"
+        content="Description goes here">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta property="og:image" content="./images/thumbnail.png" />
+    <meta property="og:title" content="Title" />
+    <meta property="og:description"
+        content="Description" />
+
+    <title>Events For All</title>
+
+    <link rel="icon" href="./images/heyHand.png">
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,700|PT+Serif:700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/bulma.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+    <!-- <script src="./vendor/smoothScroll.js"></script> -->
+
+</head>
+
+<body>
+
+    <!-- <NavBar> -->
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+            </a>
+
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="./index.php">Home</a>
+                <a class="navbar-item" href="./components.html">Events</a>
+
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">More</a>
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">How it works</a>
+                        <a class="navbar-item">Contact</a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">Report an issue</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                    <?php 
+		                if ($loggedon) {
+			            
+		                }
+		                else{
+                        echo "<a class='button is-primary'><strong>Sign up</strong></a>";
+                        echo "<a class='button is-light'>Log in</a>";
+                        }
+		                ?>   
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- </NavBar> -->
+
+
+    
+
+    <script>
+        var scroll = new SmoothScroll('a[href*="#"]', {
+            updateURL: false, // Update the URL on scroll
+            emitEvents: true, // Emit custom events
+            speed: 175 // 1.75 seconds to scroll to anchor point
+        });
+    </script>
+
+    <script src="./js/scripts.js"></script>
+</body>
+
+</html>
